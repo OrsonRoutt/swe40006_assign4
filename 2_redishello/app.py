0fdb3a731257996be02171dd55d18e2088daf61c
Orson Routt <orson@routt.net>
2025-09-30 11:15:28 +1000

    Docker python redis hello world.

---
+++ 2_redishello/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from redis import Redis, RedisError
+import os
+import socket
+
+redis = Redis(host="redis", db=0, socket_timeout=2, socket_connect_timeout=2, port=6379)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>Cannot connect to Redis, counter disabled.</i>"
+    html = "<h3>Hello {name}!</h3>" \
+            "<b>Hostname:</b> {hostname}<br/>" \
+            "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
