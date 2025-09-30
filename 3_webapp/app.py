df6ee8e8392016c9a3fb406060c013a527bf0d0f
Orson Routt <orson@routt.net>
2025-09-30 19:39:06 +1000

    Docker python web app.

--- 3_webapp/app.py
+++ 3_webapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session
+from flask import Flask, session, render_template, redirect, request
 from flask_session import Session
 from redis import Redis, RedisError
 import socket
@@ -15,17 +15,33 @@ app.config["PERMANENT_SESSION_LIFETIME"] = 1800
 
 app.config["SESSION_REDIS"] = Redis(host="redis", db=0, socket_timeout=2, socket_connect_timeout=2, port=6379)
 
+Session(app)
+
+# username: [password, message]
+users = {
+    "admin": ["admin", "You are the administrator."],
+    "orson": ["rosebud", "Hello!"],
+}
+
 @app.route("/")
 def index():
-    return "ok"
+    username = session.get("uname", None)
+    if username:
+        return render_template("index.html", uname=username, msg=users[username][1])
+    return redirect("/login")
 
-@app.route("/set/")
-def set():
-    session["key"] = "value"
-    return "ok"
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["uname"]
+        if username in users and users[username][0] == request.form["pword"]:
+            session["uname"] = username
+            return redirect("/")
+        else:
+            return render_template("login.html", error="Invalid username or password.")
+    return render_template("login.html")
 
-@app.route("/get/")
-def get():
-    return session.get("key", "not set")
-
-Session(app)
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
